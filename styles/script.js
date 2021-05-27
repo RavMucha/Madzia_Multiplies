d11a270f46a2f6719085c1457f5ec83971a385d0
Rafał Mucha <r.p.mucha@gmail.com>
2021-05-27 10:43:39 +0200

    SW script change

--- styles/script.js
+++ styles/script.js
@@ -1,12 +1,3 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("../serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
-
 var bounce = new Audio("./assets/funny.mp3");
 var dog = new Audio("./assets/doggy.mp3");
 var horse = new Audio("./assets/horse.mp3");
